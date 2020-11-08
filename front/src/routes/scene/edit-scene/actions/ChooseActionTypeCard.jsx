68e176d45582582c23f20ef11afcb474892d5af7
Christophe Bellier <chris.bellier92@gmail.com>
2020-11-08 12:56:34 +0100

    feat: add scene fade in action in front

--- front/src/routes/scene/edit-scene/actions/ChooseActionTypeCard.jsx
+++ front/src/routes/scene/edit-scene/actions/ChooseActionTypeCard.jsx
@@ -6,6 +6,7 @@ import { Text } from 'preact-i18n';
 import { ACTIONS } from '../../../../../../server/utils/constants';
 
 const ACTION_LIST = [
+  ACTIONS.LIGHT.FADE_IN,
   ACTIONS.LIGHT.TURN_ON,
   ACTIONS.LIGHT.TURN_OFF,
   ACTIONS.SWITCH.TURN_ON,
